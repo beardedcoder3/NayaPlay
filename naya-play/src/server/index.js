943660ecd0e9df1dc3f45b73b89253a5f6efe401
beardedcoder3 <beardedcoder3@gmail.com>
2024-12-23 09:06:31 +0500

    New changes

--- naya-play/src/server/index.js
+++ naya-play/src/server/index.js
@@ -41,12 +41,23 @@ const io = new Server(httpServer, {
 });
 
 // Middleware
+const allowedOrigins = process.env.ALLOWED_ORIGINS 
+  ? process.env.ALLOWED_ORIGINS.split(',') 
+  : ['https://dev.d3gazqn8zu3vg.amplifyapp.com', 'http://localhost:3002'];
+
 app.use(cors({
-  origin: process.env.ALLOWED_ORIGINS.split(','),
+  origin: (origin, callback) => {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
+
 app.use(express.json());
 
 // Email Configuration
