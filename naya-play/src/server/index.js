cd6fededda7a55c3864c8763f5223e9f80fb87c5
beardedcoder3 <beardedcoder3@gmail.com>
2025-01-11 10:01:09 +0500

    main

--- naya-play/src/server/index.js
+++ naya-play/src/server/index.js
@@ -80,7 +80,7 @@ app.use(cors({
 // Email Configuration
 const transporter = nodemailer.createTransport({
   host: 'smtp.zoho.eu',         // Changed from smtppro.zoho.eu
-  port: 587,                    
+  port: 465,                    
   secure: true,                 
   auth: {
     user: 'noreply@nayaplay.co',
